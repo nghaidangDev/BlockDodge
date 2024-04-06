c828c967839d5673addfa749d16dd5fd3394f520
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-04-06 15:32:09 +0700

    add asset, scripts player and block

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private float horizontal;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (touchPos.x < 0)
+            {
+                rb.AddForce(Vector2.left *  moveSpeed);
+            }
+            else
+            {
+                rb.AddForce(Vector2.right * moveSpeed);
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+    }
+}
